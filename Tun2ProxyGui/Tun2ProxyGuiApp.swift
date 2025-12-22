f730a75aaca5526f7adc77ad1235e60b04e455f8
Mahdi Esbati <mahdi.esbati123@gmail.com>
2025-12-22 18:55:59 +0330

    Initial Commit

---
+++ Tun2ProxyGui/Tun2ProxyGuiApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tun2ProxyGuiApp.swift
+//  Tun2ProxyGui
+//
+//  Created by Mahdi Esbati on 12/22/25.
+//
+
+import SwiftUI
+
+@main
+struct Tun2ProxyGuiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
